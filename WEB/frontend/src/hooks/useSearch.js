249f8d732390cde170aa8617449b7af75cef3309
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-10-11 10:41:54 +0000

    페이지네이션을 위한 샘플 데이터 갯수 추가

--- WEB/frontend/src/hooks/useSearch.js
+++ WEB/frontend/src/hooks/useSearch.js
@@ -8,9 +8,34 @@ import { filterListState } from '../atoms/filterListState';
 export default function useSeacrh() {
   const [searchList, setSearchList] = useRecoilState(searchListState);
   const filterList = useRecoilValue(filterListState);
+  let token = localStorage.getItem("token");
+  const data = {
+    "category": "news",
+    "period": 0,
+    "tags": {"PER": ["김정은"], "LOC": ["북한"] },
+    "search_text": "",
+    "limit": 7,
+    "offset": 0
+}
+
   /* TODO searchSetting 을 이용해서 params 넘겨주는 코드 작성 */
   useEffect(() => {
     //TODO: API 서버 배포시 수정
+
+    fetch('/api/nlp/analyze/', {
+      method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        },
+        body: JSON.stringify(data)
+      }).then(res => res.json())
+      .then(json=>{
+        console.log("=========[api/nlp/analyze/]========");
+        console.log(json);
+      });
+      
+
     const searchUrl = `/static/SecretData.example.json`;
     async function fetchSearch() {
       axios.get(searchUrl).then((data) => {
