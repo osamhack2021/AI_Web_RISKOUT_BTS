09295624061a6b4c6b287ba234d0c9744900b7fb
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-10-12 14:20:37 +0000

    로그인한 사용자만 위협탐지현황 페이지 사용 가능

--- WEB/frontend/src/hooks/useSearch.js
+++ WEB/frontend/src/hooks/useSearch.js
@@ -4,10 +4,12 @@ import axios from 'axios';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { searchListState } from '../atoms/searchListState';
 import { filterListState } from '../atoms/filterListState';
+import { useHistory } from 'react-router'
 
 export default function useSeacrh() {
   const [searchList, setSearchList] = useRecoilState(searchListState);
   const filterList = useRecoilValue(filterListState);
+  const history = useHistory()
   let token = localStorage.getItem("token");
   const data = {
     "category": "news",
@@ -21,20 +23,25 @@ export default function useSeacrh() {
   /* TODO searchSetting 을 이용해서 params 넘겨주는 코드 작성 */
   useEffect(() => {
     //TODO: API 서버 배포시 수정
-
-    fetch('/api/nlp/analyze/', {
-      method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Token ${token}`,
-        },
-        body: JSON.stringify(data)
-      }).then(res => res.json())
-      .then(json=>{
-        console.log("=========[api/nlp/analyze/]========");
-        console.log(json);
-      });
-      
+    if(token.length==0){
+      alert("로그인이 필요한 화면 입니다.");
+      history.push("/login");
+    }else{
+      fetch('/api/nlp/analyze/', {
+        method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`,
+          },
+          body: JSON.stringify(data)
+        }).then(res => res.json())
+        .then(json=>{
+          console.log("=========[api/nlp/analyze/]========");
+          console.log(json);
+        });
+        
+  
+    }
 
     const searchUrl = `/static/SecretData.example.json`;
     async function fetchSearch() {
