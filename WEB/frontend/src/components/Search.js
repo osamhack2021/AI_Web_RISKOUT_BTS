b9c5738ac63f4851c547e70329cee74bcbf4a503
Wonbin Lee <89712521+liboto00@users.noreply.github.com>
2021-10-12 02:17:39 +0000

    edit search

--- WEB/frontend/src/components/Search.js
+++ WEB/frontend/src/components/Search.js
@@ -3,12 +3,11 @@ import Box from '@mui/material/Box';
 import AutocompleteInSearch from './AutocompleteInSearch';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
-import { sampleData, sampleOptions } from './sample.js';
 
 export default function Search() {
   return (
     <Box>
-      <AutocompleteInSearch tableData={sampleData} options={sampleOptions} />
+      <AutocompleteInSearch />
     </Box>
   );
 }
