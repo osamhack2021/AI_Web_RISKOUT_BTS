f5ab19fd6d0f3e4645033274afe26d9f2cebfd14
PFF <50140505+playff@users.noreply.github.com>
2021-10-12 08:26:44 +0000

    Add color and Icon to search chip

--- WEB/frontend/src/components/SearchBar.js
+++ WEB/frontend/src/components/SearchBar.js
@@ -13,6 +13,27 @@ import {
 import useSearchEffect from '../hooks/useSearchEffect';
 
 import { Autocomplete, Box, Button, Chip, TextField } from '@mui/material';
+// mui icons
+import PersonIcon from '@mui/icons-material/Person';
+import PlaceIcon from '@mui/icons-material/Place';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+
+export function SearchChip(props) {
+  const { code } = props;
+  if (code === 'PER')
+    return <Chip color="primary" icon={<PersonIcon />} {...props} />;
+  else if (code === 'ORG')
+    return <Chip color="warning" icon={<ApartmentIcon />} {...props} />;
+  else if (code === 'LOC')
+    return <Chip color="success" icon={<PlaceIcon />} {...props} />;
+  else if (code === 'CVL')
+    return <Chip color="secondary" icon={<LocalOfferIcon />} {...props} />;
+  else if (code === 'DAT' || code === 'TIM')
+    return <Chip icon={<DateRangeIcon />} {...props} />;
+  else return <Chip {...props} />;
+}
 
 export default function SearchBar() {
   useSearchEffect();
@@ -54,8 +75,8 @@ export default function SearchBar() {
         )}
         renderTags={(value, getTagProps) =>
           value.map((option, index) => (
-            <Chip
-              color={option.label === 'PER' ? 'primary' : 'secondary'}
+            <SearchChip
+              code={option.label}
               variant="outlined"
               label={`${option.word}`}
               {...getTagProps({ index })}
