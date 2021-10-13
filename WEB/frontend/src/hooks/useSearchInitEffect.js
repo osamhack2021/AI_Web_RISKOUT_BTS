680be7195c11ecb2ad6b8de752ec98978984ed17
PFF <50140505+playff@users.noreply.github.com>
2021-10-13 03:26:54 +0000

    Do not init if search exists

--- WEB/frontend/src/hooks/useSearchInitEffect.js
+++ WEB/frontend/src/hooks/useSearchInitEffect.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 
-import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { searchState } from '../atoms/searchState';
 
 export default function useSearchInitEffect() {
-  const setSearch = useSetRecoilState(searchState);
+  const [search, setSearch] = useRecoilState(searchState);
   /* TODO searchSetting 을 이용해서 params 넘겨주는 코드 작성 */
   useEffect(() => {
+    if (Object.keys(search).length !== 0) return;
+
     //TODO: API 서버 배포시 수정
     const searchUrl = `/static/SecretData.example.json`;
     async function fetchSearch() {
