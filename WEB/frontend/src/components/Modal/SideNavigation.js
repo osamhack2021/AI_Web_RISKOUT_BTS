705a1e60c684c1dfab567d059a378f90d9e4c4a3
Wonbin Lee <89712521+liboto00@users.noreply.github.com>
2021-10-13 13:29:23 +0000

    사이드 네비 확대시 스크롤 생기는 버그 수정

--- WEB/frontend/src/components/Modal/SideNavigation.js
+++ WEB/frontend/src/components/Modal/SideNavigation.js
@@ -109,6 +109,7 @@ export default function SideNavigation(props) {
             top: 0,
             width: drawerWidth,
             height: '100vh',
+            overflow: 'hidden',
           },
         }}
       >
