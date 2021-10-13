3a5d1f2ad12c1204e7dad9473ca49292a7bc4bba
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-13 18:49:08 +0900

    * Bug fix

--- run.sh
+++ run.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+web_docker_secret="./WEB/backend/web-docker-env"
+web_drf_secret="./WEB/backend/drf/secrets.json"
+crawler_twitter_secret="./WEB/backend/crawler/crawler/model/twitter/secrets.py"
+crawler_setting="./WEB/backend/crawler/crawler/setting.py"
+crawler_site_instance="./WEB/backend/crawler/crawler/model/siteInstanceServer.py"
 
 # Check if the credential file exists.
-if [ -s "./WEB/backend/web-docker-env" ] ; then
+if [ -s $web_docker_secret ] ; then
     :
 else
-    echo "[+] The web-docker-env file does not exist or is empty!"
+    echo "[!] The '$web_docker_secret' file does not exist or is empty!"
     exit
 fi
 
-if [ -s "./WEB/backend/drf/secrets.json" ] ; then
+if [ -s $web_drf_secret ] ; then
     :
 else
-    echo "[+] The secrets.json file does not exist or is!"
+    echo "[!] The '$web_drf_secret' file does not exist or is!"
     exit
 fi
 
-if [ -s "./WEB/backend/crawler/crawler/model/twitter/secrets.py" ] ; then
+if [ -s $crawler_twitter_secret ] ; then
     :
 else
-    echo "[+] The secrets.py file does not exist or is empty!"
+    echo "[!] The '$crawler_twitter_secret' file does not exist or is empty!"
     while true; do
         read -p "Do you want to exclude Twitter from crawling list? (Y/N) : " yn
         case $yn in
-            [Yy]* ) sed -i "s/'Twitter': 'twitter'/# 'Twitter': 'twitter'/" ./WEB/backend/crawler/crawler/setting.py; break;;
+            [Yy]* )
+                break;;
             [Nn]* ) exit;;
             * ) echo "Please answer yes or no.";;
         esac
@@ -45,34 +51,33 @@ echo [+] NLP Server started!
 echo [+] Building Analyzer Server...
 cd ./WEB/backend
 sudo docker-compose -f analyzer-docker-compose.yml up --build -d
+cd ../
+
 
 # Wait until crawling and analyzing done
-until [ "`docker inspect -f {{.State.Status}} analyzer_service`"=="exited" && "`docker inspect -f {{.State.ExitCode}} analyzer_service`"=="0"]; do
-    sleep 1;
+
+echo [+] Wait until crawling and analyzing done... \(This might take tens of minutes to complete.\)
+until [ "`sudo docker inspect -f {{.State.Status}} analyzer_service`" = "exited" ]; do
+    sleep 5;
 done;
-echo [+] Analyzer Server started!
+echo [+] Crawling and analyzing done!
 
 
 # Build React
 
 echo [+] Building React...
-
 mkdir -p ./drf/build
 mkdir -p ./drf/staticfiles
-
 cd ./frontend
-
 npm i
 npm run build
-
 rm -rf ../backend/drf/build/*
 cp -r ./build ../backend/drf/
+cd ../backend
 
 
 # Build & Run Django Server
 
 echo [+] Building Django server...
-cd ../backend
-
 sudo docker-compose -f web-docker-compose.yml --env-file web-docker-env up --build -d
 echo [+] Django Server started!
