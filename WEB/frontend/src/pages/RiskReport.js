eea2fb4be99bf41fc46fd0013293be6599704ebf
Wonbin Lee <89712521+liboto00@users.noreply.github.com>
2021-10-15 05:32:02 +0000

    loading skeleton 수정

--- WEB/frontend/src/pages/RiskReport.js
+++ WEB/frontend/src/pages/RiskReport.js
@@ -78,65 +78,26 @@ const RiskReport = (props) => {
 
   const loadingScreen = (
     <section id="sub_contents" style={{ width: '100vw', height: '100vh' }}>
-      <div className="sub01_wrap">
+      <div className="sub01_wrap" style={{ marginBottom: 40 }}>
         <h2 className="h2_tit2">보고서 생성 중...</h2>
       </div>
       <div className="content clfix">
-        <Skeleton animation="wave" height={75} width="10%" />
-        <Skeleton animation="wave" height={75} width="15%" />
-        <Skeleton animation="wave" height={15} width="40%" />
+        <Skeleton animation="wave" height={45} width="45%" />
+        <Skeleton animation="wave" height={35} width="40%" />
+        <Skeleton animation="wave" height={30} width="35%" />
         <Skeleton
           animation="wave"
-          height={15}
-          width="30%"
+          height={30}
+          width="20%"
           style={{ marginBottom: 26 }}
         />
-        <Skeleton animation="wave" height={60} width="14%" />
-
-        {/* 날짜 버튼 */}
-        <Box sx={{ display: 'flex' }}>
-          <Skeleton
-            sx={{ borderRadius: '10px', marginRight: '13px' }}
-            width={120}
-            height={45}
-            animation="wave"
-            variant="rectangular"
-          />
-          <Skeleton
-            sx={{ borderRadius: '10px', marginRight: '13px' }}
-            width={120}
-            height={45}
-            animation="wave"
-            variant="rectangular"
-          />
-          <Skeleton
-            sx={{ borderRadius: '10px', marginRight: '13px' }}
-            width={120}
-            height={45}
-            animation="wave"
-            variant="rectangular"
-          />
-          <Skeleton
-            sx={{ borderRadius: '10px', marginRight: '13px' }}
-            width={120}
-            height={45}
-            animation="wave"
-            variant="rectangular"
-          />
-          <Skeleton
-            sx={{ borderRadius: '10px', marginRight: '13px' }}
-            width={120}
-            height={45}
-            animation="wave"
-            variant="rectangular"
-          />
-        </Box>
+        
         <Box sx={{ display: 'flex' }}>
           <Box>
             {/* 일일 리스크 현황 */}
             <Skeleton
               sx={{
-                borderRadius: '10px',
+                borderRadius: '5px',
                 marginTop: '13px',
                 marginRight: '33px',
               }}
@@ -146,82 +107,19 @@ const RiskReport = (props) => {
               variant="rectangular"
             />
           </Box>
-
           <Box>
-            {/* 일일 리스크 현황 옆 기사 */}
-            {Array.from({ length: 3 }).map((_, i) => (
-              <Box>
-                <Skeleton key={i} animation="wave" width={500} height={50} />
-                <Skeleton key={i} animation="wave" width={300} height={15} />
-                <Skeleton key={i} animation="wave" width={350} height={15} />
-                <Skeleton key={i} animation="wave" width={250} height={15} />
-                <Skeleton
-                  key={i}
-                  animation="wave"
-                  width={400}
-                  height={15}
-                  sx={{ marginBottom: '7px' }}
-                />
-
-                {/* chip */}
-                <Box sx={{ display: 'flex' }}>
-                  <Skeleton
-                    sx={{ borderRadius: '20px', marginRight: 1 }}
-                    width={53}
-                    height={30}
-                    animation="wave"
-                    variant="rectangular"
-                  />
-                  <Skeleton
-                    sx={{
-                      borderRadius: '20px',
-                      marginRight: 1,
-                      marginBottom: '13px',
-                    }}
-                    width={53}
-                    height={30}
-                    animation="wave"
-                    variant="rectangular"
-                  />
-                </Box>
-              </Box>
-            ))}
+            <Skeleton
+            sx={{ borderRadius: '5px', marginTop: '13px' }}
+            width={879}
+            height={450}
+            animation="wave"
+            variant="rectangular"
+            />
           </Box>
         </Box>
 
         {/* 리스크 종류별 비율 (%) */}
-        <Skeleton
-          sx={{ borderRadius: '10px', marginTop: 2 }}
-          width={879}
-          height={400}
-          animation="wave"
-          variant="rectangular"
-        />
-
-        {/* 중대 위협 */}
-        <Skeleton
-          sx={{ borderRadius: '10px', marginTop: 10 }}
-          animation="wave"
-          height={95}
-          width="15%"
-        />
-        <Box sx={{ display: 'flex' }}>
-          {Array.from({ length: 4 }).map((_, i) => (
-            <Skeleton
-              sx={{
-                borderRadius: '10px',
-                marginLeft: 9,
-                marginRight: 10,
-                marginTop: 5,
-                marginBottom: 7,
-              }}
-              width={300}
-              height={324}
-              animation="wave"
-              variant="rectangular"
-            />
-          ))}
-        </Box>
+        
       </div>
     </section>
   );
@@ -243,111 +141,7 @@ const RiskReport = (props) => {
 
   return (
     <>
-      {isPending ? (
-        loadingScreen
-      ) : error ? (
-        errorScreen
-      ) : (
-        <section id="sub_contents" ref={pdfExportComponent}>
-          <PdfExportButton exportTarget={pdfExportComponent} />
-          <Box className="sub01_wrap">
-            <Grid container spacing={1} direction="column">
-              <Grid item>
-                <Typography variant="h2">
-                  Risk Report{' '}
-                  <em style={{ fontSize: '0.5em' }}>
-                    {new Intl.DateTimeFormat('ko-KR', {
-                      dateStyle: 'full',
-                    }).format(new Date())}{' '}
-                    (24h)
-                  </em>
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>{getLineBreakText(data.overview)}</Typography>
-              </Grid>
-            </Grid>
-
-            <Grid container spacing={1} mt={3} direction="column">
-              <Grid item>
-                {/* <Typography variant="h3">리스크 브리핑</Typography> */}
-              </Grid>
-              <Grid item>
-                <ExclusiveSelect
-                  selectOptions={['1d', '1wk', '1m', '1yr', 'all']}
-                  selectedValue={dateRange}
-                  setSelectedValue={setDateRange}
-                  selectHandler={selectHandler}
-                />
-              </Grid>
-
-              <Grid
-                item
-                container
-                spacing={5}
-                mt={1}
-                direction="row"
-                justifyContent="space-between"
-                alignItems="flex-start"
-              >
-                <Grid item xs={12} md={6}>
-                  <Graphs data={data.briefingGraphData} />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <Grid container direction="column" spacing={3}>
-                    {data.briefingContents.map((props, i) => {
-                      return (
-                        <ScrappedArticle key={'scrapped' + i} {...props} />
-                      );
-                    })}
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid
-              container
-              className="content clfix"
-              direction="column"
-              spacing={3}
-              mt={1}
-            >
-              <Grid item>
-                <Typography variant="h3">중대 위협</Typography>
-              </Grid>
-              <Grid
-                item
-                container
-                direction="row"
-                justifyContent="space-evenly"
-                alignItems="center"
-                sx={{ mt: '1rem' }}
-              >
-                {data.majorEvents.map(
-                  ({
-                    imageUrl,
-                    title,
-                    threatType,
-                    sourceName,
-                    url,
-                    datetime,
-                  }) => (
-                    <ThreatMediaCard
-                      imageUrl={imageUrl}
-                      title={title}
-                      threatType={threatType}
-                      sourceName={sourceName}
-                      url={url}
-                      datetime={datetime}
-                      key={title}
-                    />
-                  )
-                )}
-              </Grid>
-            </Grid>
-          </Box>
-        </section>
-      )}
+      {loadingScreen}
     </>
   );
 };
