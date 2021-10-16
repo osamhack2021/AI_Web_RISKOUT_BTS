807e3e783f8e2a34375005bd881ffc3043b2d067
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-10-16 14:31:07 +0000

    Edit API Request

--- WEB/frontend/src/components/Modal/PasswordResetModal.js
+++ WEB/frontend/src/components/Modal/PasswordResetModal.js
@@ -51,7 +51,78 @@ const handlePassword2Change = (e) => {
  };
  //Edit API Request
     return(
-     
+      <Box className="loginModalBack">
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="left">
+          {/* <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar> */}
+          <h1 style={{ fontSize: '32px' }}>비밀번호 초기화</h1>
+        </Grid>
+        <Grid align="center">
+          <Box
+            sx={{ width: 314, height: 50, marginBottom: '1.2em', marginTop: 5 }}
+          >
+            <TextField
+              label="새 비밀번호"
+              placeholder="비밀번호를 입력 해 주세요."
+              fullWidth
+              required
+              onChange={handleNameChange}
+              variant="outlined"
+            />
+          </Box>
+          <Box sx={{ width: 314, height: 50, marginBottom: '2em' }}>
+            <TextField
+              label="새 비밀번호 확인"
+              placeholder="새 비밀번호를 다시 입력해 주세요."
+              fullWidth
+              required
+              variant="outlined"
+              onChange={handlePasswordChange}
+            />
+          </Box>
+          {/* */}
+          <Link sx={{ textDecoration: 'none'}}>
+            <Button
+              type="submit"
+              variant="contained"
+              style={btnstyle}
+              className="JoinLoign-button"
+
+              onClick={(e)=>{
+                e.preventDefault()
+                fetch('/api/user/login/', {
+                method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(data)
+                })
+                .then(res => res.json())
+                .then(json => {
+                  if (json.token) {
+                    props.userHasAuthenticated(true, data.username, json.token);
+                    alert("환영합니다."+username+"님.")
+                    history.push("/");
+                    props.setModal(true)
+                    console.log(json)
+                  }else{
+                    alert("아이디 또는 비밀번호를 확인해주세요.")
+                  }
+                })
+                .catch(error => alert(error));
+              }} 
+            >
+              변경
+            </Button >
+          </Link>
+        </Grid>
+        <Typography align="center" variant="subtitle1">
+          <Link href="/register" sx={{ textDecoration: 'none', color: 'rgb(113,137,218)' }}>
+            RISKOUT 회원이 아닌가요? 지금 가입하세요.
+          </Link>
+        </Typography>
+      </Paper>
+    </Box>
     );
 }
 
