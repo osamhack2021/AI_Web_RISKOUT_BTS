12f6a93b9935f3ae819dd6bbcb4a34e459eeedbf
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-10-16 14:29:13 +0000

    PasswordResetPage Styling

--- WEB/frontend/src/components/Modal/PasswordResetModal.js
+++ WEB/frontend/src/components/Modal/PasswordResetModal.js
@@ -50,53 +50,7 @@ const handlePassword2Change = (e) => {
   color: 'white'
  };
     return(
-      <>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Grid>
-      <Paper elevation={10} style={paperStyle}>
-          <Grid align='center'>
-               <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-              <h2>비밀번호 찾기</h2>
-          </Grid>
-          <TextField label='새 비밀번호' placeholder='새 비밀번호를 입력해 주세요.' type='password' fullWidth required  onChange={handlePassword1Change}/>
-          <TextField label='새 비밀번호 확인' placeholder='새 비밀번호를 다시 입력해 주세요.' type='password' fullWidth required  onChange={handlePassword2Change}/>
-          <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth className="JoinLoign-button" onClick={(e)=>{
-                  e.preventDefault()
-                  alert(JSON.stringify(data))
-                  if(userpassword !== userpasswordCheck){
-                    alert("비밀번호가 서로 일치하지 않습니다!")
-                  }else{
-                    fetch('/api/password-reset/confirm/', {
-                      method: 'POST',
-                      headers:{
-                        'Content-Type': 'application/json'
-                      },
-                      body: JSON.stringify(data)
-                    }).then(res => res.json())
-                    .then(json => {
-                      if(json.status==="OK"){
-                          alert("성공적으로 변경되었습니다.");
-                          history.push("/");
-                      }else{
-                          alert("비밀번호를 다시 확인 해 주세요.")
-                      }
-                    })
-                    .catch(error => alert(error));
-                    };
-
-                  }
-                  }>전송</Button>
-          <Typography >
-          <br></br>
-               <Link href="/login" >
-                  로그인
-          </Link>
-          </Typography>
-      </Paper>
-  </Grid>
-  </>
+     
     )
 }
 
