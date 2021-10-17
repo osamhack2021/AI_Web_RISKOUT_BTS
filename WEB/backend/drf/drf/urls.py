2bac3527171c6de02b85734f5b3b7a0d7fa0e806
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-17 00:05:18 +0000

    * Add main page renderer

--- WEB/backend/drf/drf/urls.py
+++ WEB/backend/drf/drf/urls.py
@@ -3,11 +3,16 @@ from django.urls import path, re_path, include
 from django.shortcuts import render
 
 
+def render_main(request):
+    return render(request, "main.html")
+
+
 def render_react(request):
     return render(request, "index.html")
 
 
 urlpatterns = [
+    path('/', render_main),
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
     path('api/user/', include('accounts.urls')),
