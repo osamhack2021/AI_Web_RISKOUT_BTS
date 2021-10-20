f727bda5b3941ecbd752a8e7c81686c9c844086f
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-20 06:36:39 +0000

    * Fix Typo

--- WEB(FE)/frontend/src/components/Demo.js
+++ WEB(FE)/frontend/src/components/Demo.js
@@ -5,7 +5,7 @@ export default function Demo(demoPaddingTop, demoPaddingLeft) {
   return (
     <Box sx={{ display: "flex", marginBottom: "3px" }}>
       <Chip label="Beta" color="info" sx={{ color: "white", fontWeight: "bold" }} />
-      <h4 style={{ color: "darkgray", marginLeft: "7px", marginTop: "5px" }}>본 페이지는 BETA 테스트 시범운영중으로 일부 오류가 발생할 수 있습니다.</h4>
+      <h4 style={{ color: "darkgray", marginLeft: "7px", marginTop: "5px" }}>본 페이지는 BETA 테스트 시범운영 중으로 일부 오류가 발생할 수 있습니다.</h4>
     </Box>
   );
 }
