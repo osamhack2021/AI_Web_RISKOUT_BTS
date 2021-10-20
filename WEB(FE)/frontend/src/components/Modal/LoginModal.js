dac29ad8441ab95a1ba5aa221bca963cd9dee6ca
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-20 03:00:39 +0000

    * 회원가입 기능 비활성화

--- WEB(FE)/frontend/src/components/Modal/LoginModal.js
+++ WEB(FE)/frontend/src/components/Modal/LoginModal.js
@@ -51,6 +51,10 @@ const LoginModal = (props) => {
     color: 'white',
   };
 
+  const onClick = () => {
+    alert('데모 버전에서는 제공하지 않는 기능입니다')
+  };
+
   return (
     <Box className="loginModalBack">
       <Paper elevation={10} style={paperStyle}>
@@ -130,7 +134,8 @@ const LoginModal = (props) => {
         {/* <br /> */}
         <Typography align="center" variant="subtitle1">
           <Link
-            href="/register"
+            // href="/register"
+            onClick={onClick}
             sx={{ textDecoration: 'none', color: '#3a8ffb' }}
           >
             RISKOUT 회원이 아닌가요? 지금 가입하세요.
